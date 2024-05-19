58b42028dc86ff8a201f3487579a832a0a759d6e
mepatrick73 <pameu17@ulaval.ca>
2024-05-19 18:58:51 -0400

    working vector addition kernel

--- wgpu/vector_addition/src/main.rs
+++ wgpu/vector_addition/src/main.rs
@@ -1,10 +1,26 @@
+use wgpu::{util::DeviceExt, ComputePipelineDescriptor, PipelineLayoutDescriptor};
+
+const SHADER: &str = r#"
+@group(0) @binding(0) var<storage, read> a : array<f32>;
+@group(0) @binding(1) var<storage, read> b : array<f32>;
+@group(0) @binding(2) var<storage, read_write> c : array<f32>;
+
+@compute @workgroup_size(256)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    let index = global_id.x;
+    if index < arrayLength(&a) {
+        c[index] = a[index] + b[index];
+    }
+}
+"#;
+
 async fn run() {
     let instance_descriptor = wgpu::InstanceDescriptor {
         backends: wgpu::Backends::PRIMARY,
         ..Default::default()
     };
     let instance = wgpu::Instance::new(instance_descriptor);
-    let _adapter = instance
+    let adapter = instance
         .request_adapter(&wgpu::RequestAdapterOptions {
             power_preference: wgpu::PowerPreference::HighPerformance,
             compatible_surface: None,
@@ -12,6 +28,170 @@ async fn run() {
         })
         .await
         .expect("Failed to find an appropriate adapter");
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+                label: None,
+            },
+            None,
+        )
+        .await
+        .expect("Failed to create device");
+
+    let shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: Some("Compute Shader"),
+        source: wgpu::ShaderSource::Wgsl(SHADER.into()),
+    });
+
+    let data_size = 10000;
+    let buffer_size = (data_size * std::mem::size_of::<f32>()) as wgpu::BufferAddress;
+
+    let a_data: Vec<f32> = (0..data_size).map(|x| x as f32).collect();
+    let b_data: Vec<f32> = (0..data_size).map(|x| x as f32).collect();
+
+    let a_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("A Buffer"),
+        contents: bytemuck::cast_slice(&a_data),
+        usage: wgpu::BufferUsages::STORAGE,
+    });
+
+    let b_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("B Buffer"),
+        contents: bytemuck::cast_slice(&b_data),
+        usage: wgpu::BufferUsages::STORAGE,
+    });
+
+    let c_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("C Buffer"),
+        size: buffer_size,
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+
+    // Create bind group layout and bind group
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 2,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+        ],
+        label: Some("bind_group_layout"),
+    });
+
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: a_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: b_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: c_buffer.as_entire_binding(),
+            },
+        ],
+        label: Some("bind_group"),
+    });
+
+    let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+        label: Some("compute_pipeline"),
+        bind_group_layouts: &[&bind_group_layout],
+        push_constant_ranges: &[],
+    });
+
+    let compute_pipeline = device.create_compute_pipeline(&ComputePipelineDescriptor {
+        label: Some("compute_pipeline"),
+        layout: Some(&pipeline_layout),
+        module: &shader_module,
+        entry_point: "main",
+        compilation_options: Default::default(),
+    });
+
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("command_encoder"),
+    });
+
+    // wrap in scope so we can get back the encoder hehe
+    {
+        let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: Some("compute_pass"),
+            timestamp_writes: None,
+        });
+        compute_pass.set_pipeline(&compute_pipeline);
+        compute_pass.set_bind_group(0, &bind_group, &[]);
+        compute_pass.dispatch_workgroups((data_size as u32 + 255) / 256, 1, 1);
+    }
+
+    let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("staging_buffer"),
+        size: buffer_size,
+        usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
+    encoder.copy_buffer_to_buffer(&c_buffer, 0, &staging_buffer, 0, buffer_size);
+
+    queue.submit(Some(encoder.finish()));
+
+    let buffer_slice = staging_buffer.slice(..);
+
+    let (sender, receiver) = flume::bounded(1);
+    let _buffer_future =
+        buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+    device.poll(wgpu::Maintain::wait()).panic_on_timeout();
+
+    let vec: Vec<f32> = if let Ok(Ok(())) = receiver.recv_async().await {
+        let data = buffer_slice.get_mapped_range();
+        let result = bytemuck::cast_slice(&data).to_vec();
+
+        drop(data);
+        staging_buffer.unmap();
+
+        result
+    } else {
+        panic!("failed to run compute on gpu!")
+    };
+
+    for i in 0..data_size {
+        assert_eq!(vec[i], a_data[i] + b_data[i]);
+    }
+
+    println!("Vector addition kernel successful");
 }
 
 fn main() {
